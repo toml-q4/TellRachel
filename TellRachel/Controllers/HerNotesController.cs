1165fa56c08142e0698001b73844f3226dd76d98
Tom Luong <toml@q4inc.com>
2017-07-27 09:47:36 -0400

    Add new controller HerNotes

---
+++ TellRachel/Controllers/HerNotesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TellRachel.Controllers
+{
+    [Route("api/hernotes")]
+    public class HerNotesController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult GetNotes()
+        {
+            return Ok(new List<object> {
+                new
+                {
+                    id = 1,
+                    content = Guid.NewGuid().ToString()
+                }
+            });
+        }
+    }
+}
