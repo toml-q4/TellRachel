9d609a02f436a2c9ac0baf4f37b249af35b17ee8
Tom Luong <toml@q4inc.com>
2017-07-29 07:47:16 -0400

    Add repo

---
+++ TellRachel/Repositories/ITellRachelRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TellRachel.Entities;
+
+namespace TellRachel.Repositories
+{
+    public interface ITellRachelRepository
+    {
+        IEnumerable<HerNote> GetHerNotes();
+
+        HerNote GetHerNote(int herNoteId, bool includeHerRecords);
+
+        IEnumerable<HerRecord> GetHerRecords(int herNoteId);
+
+        HerRecord GetHerRecord(int herNoteId, int herRecordId);
+    }
+}
